2df28f575432064cdbb86ca795524c0b42e9c2ce
adilsaid64 <adilsaid64@gmail.com>
2023-12-30 23:47:16 +0000

    fix duplicate logger

--- src/logger_config.py
+++ src/logger_config.py
@@ -4,14 +4,19 @@ def setup_logger():
     logger = logging.getLogger('RedditLogger')
     logger.setLevel(logging.DEBUG)
 
-    console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler('project.log')
+    # Check if the logger already has handlers
+    if not logger.handlers:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.DEBUG)  # Optional: Set log level for this handler
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    console_handler.setFormatter(formatter)
-    file_handler.setFormatter(formatter)
+        file_handler = logging.FileHandler('project.log')
+        file_handler.setLevel(logging.DEBUG)  # Optional: Set log level for this handler
 
-    logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler.setFormatter(formatter)
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(console_handler)
+        logger.addHandler(file_handler)
 
     return logger
