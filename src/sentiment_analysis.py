2a1fe840dfb8b019da4e1e8b7a3115d7ce1027c8
adilsaid <adilsaid64@gmail.com>
2024-01-02 18:15:38 +0000

    update

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -1,14 +1,11 @@
 from transformers import DistilBertTokenizer, TFDistilBertForSequenceClassification
 from transformers import pipeline
 
-model_name = "distilbert-base-uncased-finetuned-sst-2-english"
-model = TFDistilBertForSequenceClassification.from_pretrained(model_name)
-tokenizer = DistilBertTokenizer.from_pretrained(model_name)
-
-nlp_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+model_checkpoint = "distilbert-base-uncased-finetuned-sst-2-english"
+distil_bert_model = pipeline(task="sentiment-analysis", model=model_checkpoint)
 
 
-def get_sentiment(df, text_col, nlp_pipeline = nlp_pipeline):
+def get_sentiment(df, text_col, distil_bert_model = distil_bert_model):
     '''
     Gets sentiment on a dataframe col
     
@@ -17,5 +14,10 @@ def get_sentiment(df, text_col, nlp_pipeline = nlp_pipeline):
     
     :returns df (pd.DataFrame) : The dataframe with a new column containing sentiment.
     '''
-    df[f'sentiment_{text_col}'] = df[text_col].apply(lambda x: nlp_pipeline(x))
+    df[f'sentiment_{text_col}'] = df[text_col].apply(lambda x: distil_bert_model(x))
+
+    df[f'sentiment_{text_col}_label'] = df[f'sentiment_{text_col}'].apply(lambda x: x[0]['label'])
+    df[f'sentiment_{text_col}_score'] = df[f'sentiment_{text_col}'].apply(lambda x: x[0]['score'])
+    df.drop(columns = f'sentiment_{text_col}', inplace = True)
+    
     return df
