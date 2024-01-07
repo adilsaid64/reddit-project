0801b57542585f25a49a5465a40dfe2a87f0a6c6
adilsaid <adilsaid64@gmail.com>
2024-01-07 13:57:18 +0000

    streams reddit data to s3, stores data in bucket

--- src/get_reddit_data.py
+++ src/get_reddit_data.py
@@ -8,7 +8,6 @@ from .logger_config import setup_logger
 logger = setup_logger()
 
 logger.info('Getting Reddit Credentials')
-
 reddit_cred_file = 'src/reddit_cred.json'
 with open(reddit_cred_file, 'r') as file:
     reddit_cred = json.load(file)
@@ -16,24 +15,44 @@ with open(reddit_cred_file, 'r') as file:
 client_id = reddit_cred['client_id']
 client_secret = reddit_cred['client_secret']
 user_agent = reddit_cred['user_agent']
-
 reddit = praw.Reddit(client_id=client_id,
                      client_secret=client_secret,
                      user_agent=user_agent)
+logger.info('Got Reddit Credentials')
 
-logger.info('Got Credentials')
+logger.info('Getting AWS Credentials')
+aws_cred_file = 'src/aws_cred.json'
+with open(aws_cred_file, 'r') as file:
+    aws_creds = json.load(file)
 
+session_id_details = {
+        'access_id': aws_creds['access_id'],
+        'secret_token': aws_creds['secret_token'],
+        'session_token': aws_creds['session_token']
+}
+logger.info('Got AWS Credentials')
 
-def stream_to_s3(bucket_name, s3_key_prefix, data):
-    s3_client = boto3.client('s3')
+def stream_to_s3(bucket_name, s3_key_prefix, data, session_id_details = session_id_details):
+    access_id = session_id_details.get('access_id')
+    secret_token = session_id_details.get('secret_token')
+    session_token = session_id_details.get('session_token')
+    session = boto3.Session(
+        aws_access_key_id=access_id,
+        aws_secret_access_key=secret_token,
+        aws_session_token=session_token
+    )
+
+    s3_client = session.client('s3')
+
     s3_key = f"{s3_key_prefix}/{s3_key_prefix}_{int(time.time())}.json"
+
     buffer = io.BytesIO()
     buffer.write(json.dumps(data).encode())
     buffer.seek(0)
     s3_client.upload_fileobj(buffer, Bucket=bucket_name, Key=s3_key)
 
 def get_post_data(subreddit_name, post_limit = 100, comment_limmit = 100, reddit = reddit):
-    logger.info(f'Getting Reddit Data: {subreddit_name}')
+    logger.info(f'Getting Reddit Data: Subreddit: {subreddit_name} --- Number of Posts: {post_limit} --- Comment Limit : {comment_limmit}')
     subreddit = reddit.subreddit(subreddit_name)
     top_posts = subreddit.top(limit=post_limit)  
     posts_with_comments = []
@@ -65,9 +84,9 @@ def get_post_data(subreddit_name, post_limit = 100, comment_limmit = 100, reddit
             'subreddit': str(post.subreddit),
             'comments': comments
         }
-        #posts_with_comments.append(post_data)
+        posts_with_comments.append(post_data)
 
-        stream_to_s3('reddit-project-data', 'subreddit_name', post_data)
+        stream_to_s3('reddit-project-data', subreddit_name, post_data)
 
 
     logger.info('Got Reddit Data')
